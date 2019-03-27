42ca20745ba81130c43fed57c58227fbdaa46314
AnojanS <asaba091@uottawa.ca>
2019-03-26 23:02:12 -0400

    final changes to game flow

--- Terminal_Hacker/Assets/Hacker.cs
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
-    //Game Configuration Data
-    string[] easyPasswords = { "books", "aisle", "self", "password", "font", "borrow" };
-    string[] hardPasswords = { "prisoner", "handcuffs", "holster", "uniform", "arrest" };
 
-    //Game State
-    string level; //member variable
-    enum Screen { MainMenu, Password, Win}; //screen setting
+    // Game configuration data
+    const string menuHint = "Type 'menu' to go back to menu screen";
+    string[] level1Passwords = { "books", "aisle", "shelf", "password", "pencil", "borrow" };
+    string[] level2Passwords = { "prisoner", "handcuffs", "warden", "uniform", "arrest" };
+    string[] level3Passwords = { "starfield", "universe", "atmosphere", "exploration", "astronaut" };
+
+    // Game state
+    int level;
+    enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
     string password;
 
-    // Start is called before the first frame update
-    void Start() {
+    // Use this for initialization
+    void Start()
+    {
         ShowMainMenu();
     }
 
-    void ShowMainMenu() {
+    void ShowMainMenu()
+    {
         currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
-        Terminal.WriteLine("Hello! Choose your hacking difficulty");
-        Terminal.WriteLine("Press 1 for Easy");
-        Terminal.WriteLine("Press 2 for Hard");
-        Terminal.WriteLine("Enter your choice: ");
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for library");
+        Terminal.WriteLine("Press 2 for police station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("Enter your choice:");
     }
 
     void OnUserInput(string input)
@@ -34,54 +40,122 @@ public class Hacker : MonoBehaviour
         {
             ShowMainMenu();
         }
-        else if (currentScreen == Screen.MainMenu){
+        else if (input == "quit" || input == "close" || input == "exit")
+        {
+            Terminal.WriteLine("If on the web close the tab.");
+            Application.Quit();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
             RunMainMenu(input);
         }
-        else if (currentScreen == Screen.Password) {
+        else if (currentScreen == Screen.Password)
+        {
             CheckPassword(input);
         }
     }
 
     void RunMainMenu(string input)
     {
-        if (input == "1"){
-            level = "Easy";
-            password = easyPasswords[2];
-            StartGame();
-        }
-        else if (input == "2"){
-            level = "Hard";
-            password = hardPasswords[2];
-            StartGame();
-        }
-        else if (input == "007"){
-            Terminal.WriteLine("Please select a level Mr Bond!");
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
+            AskForPassword();
         }
         else
         {
-            Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine("Please enter valid level");
+            Terminal.WriteLine(menuHint);
         }
     }
 
-    void StartGame() {
-        currentScreen = Screen.Password;
-        Terminal.WriteLine("You have chosen " + level + " mode");
-        Terminal.WriteLine("Please enter your password: ");
-    }
-
-    void CheckPassword(string input) { 
-
-        if (input == password) {
-            Terminal.WriteLine("Congratulations, you win!");
-        }
-        else {
-            Terminal.WriteLine("Wrong password. Please try again");
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
+    void AskForPassword()
     {
-        
+        currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Enter the password (hint: " + password.Anagram()+")");
+        Terminal.WriteLine(menuHint);
+    }
+
+    void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            DisplayWinScreen();
+        }
+        else
+        {
+            AskForPassword();
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Here's your book!");
+                Terminal.WriteLine(@"
+    _______
+   /      //
+  /      //
+ /_____ //
+(______(/           
+"
+                );
+                break;
+            case 2:
+                Terminal.WriteLine("You got the prison key!");
+                Terminal.WriteLine("Play again for a greater challenge.");
+                Terminal.WriteLine(@"
+ __
+/0 \_______
+\__/-=' = '         
+"
+                );
+                break;
+            case 3:
+                Terminal.WriteLine(@"
+ _ __   __ _ ___  __ _
+| '_ \ / _` / __|/ _` |
+| | | | (_| \__ \ (_| |
+|_| |_|\__,_|___)\__,_|
+"
+                );
+                Terminal.WriteLine("Welcome to NASA's internal system!");
+                break;
+            default:
+                Debug.LogError("Invalid level reached");
+                break;
+        }
     }
 }
