0057164abe989aa0c46baaa1ce0cab6979e17c45
AnojanS <asaba091@uottawa.ca>
2019-03-26 20:49:19 -0400

    added member variable

--- Terminal_Hacker/Assets/Hacker.cs
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    //Game State
+    string level; //member variable
+
     // Start is called before the first frame update
-    void Start(){
-        ShowMainMenu("Hello player");
+    void Start() {
+        ShowMainMenu();
     }
 
-    void ShowMainMenu(string greeting) {
+    void ShowMainMenu() {
         Terminal.ClearScreen();
-        Terminal.WriteLine(greeting);
-        Terminal.WriteLine("What woud you like to hack into?");
-        Terminal.WriteLine("Press 1 for the local library.");
-        Terminal.WriteLine("Press 2 for the police station.");
+        Terminal.WriteLine("Hello! Choose your difficulty");
+        Terminal.WriteLine("Press 1 for Easy");
+        Terminal.WriteLine("Press 2 for Hard");
         Terminal.WriteLine("Enter your choice: ");
     }
 
     void OnUserInput(string input) {
-        print("User inputed " + input);
+        if(input == "menu") {
+            ShowMainMenu();
+        }
+        else if (input == "1") {
+            level = "Easy";
+            StartGame();
+        }
+        else if (input == "2") {
+            level = "Hard";
+            StartGame();
+        }
+        else {
+            Terminal.WriteLine("Please enter a valid input");
+        }
+    }
+
+    void StartGame() {
+        Terminal.WriteLine("You have chosen " + level + " mode"); 
     }
 
     // Update is called once per frame
